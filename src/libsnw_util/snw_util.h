4131223a02bdc7364ede4c42867886c4df781dac
Sojourn <jsrausch.sojourn@gmail.com>
2018-07-14 12:33:43 -0500

    alias int128_t and int256_t types

--- src/libsnw_util/snw_util.h
+++ src/libsnw_util/snw_util.h
@@ -1,4 +1,31 @@
 #pragma once
 
 #include "types.h"
-#include "intrusive_list.h"
+
+namespace snw {
+
+#if 0 // test...
+template<typename T>
+inline T align_up(T value, size_t alignment)
+{
+    auto n = reinterpret_cast<uintptr_t>(value);
+    auto aligned_n = (((alignment) + ((n)-1)) & ~((n)-1));
+    return reinterpret_cast<T>(aligned_n);
+}
+
+template<typename T>
+inline T align_down(T value, size_t alignment)
+{
+    auto n = reinterpret_cast<uintptr_t>(value);
+    auto aligned_n = ((alignment) & ~((n)-1));
+    return reinterpret_cast<T>(aligned_n);
+}
+
+template<typename T>
+inline bool is_power_of_2(T value)
+{
+    return ((value & (value - 1)) == 0) && (value != 0);
+}
+#endif
+
+}
--- src/libsnw_util/types.h
+++ src/libsnw_util/types.h
@@ -2,3 +2,10 @@
 
 #include <cstdint>
 #include <cstddef>
+
+#include <immintrin.h>
+
+namespace snw {
+    using int128_t = __m128i;
+    using int256_t = __m256i;
+}
