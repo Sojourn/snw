3418ee5357faf310035a77041ef1e7941c80b363
Joel Rausch <jsrausch.sojourn@gmail.com>
2018-08-02 18:27:40 -0500

    Add a scatter gather core example

--- src/snw/main.cpp
+++ src/snw/main.cpp
@@ -1,90 +1,151 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "snw_util.h"
 #include "snw_core.h"
 
-struct allocation_event {
-    void* addr;
-    size_t size;
+struct status_event_request {
 };
 
-struct deallocation_event {
-    void* addr;
-    size_t size;
+enum class status_event_severity {
+    warning,
+    error
 };
 
-template<typename Core>
-class memory_module : public snw::module<Core, memory_module> {
-    using module = snw::module<Core, memory_module>;
-public:
-    memory_module(Core& core)
-        : module(core)
-    {
-    }
-
-    void* allocate(size_t size) {
-        void* addr = ::malloc(size);
-        this->emit_event(allocation_event{
-            addr,
-            size
-        });
-        return addr;
-    }
-
-    void deallocate(void* addr, size_t size) {
-        this->emit_event(deallocation_event{
-            addr,
-            size
-        });
-        ::free(addr);
-    }
+struct status_event {
+    status_event_severity severity;
+    std::string           msg;
 };
 
 template<typename Core>
-class memory_audit_module : public snw::module<Core, memory_audit_module> {
-    using module = snw::module<Core, memory_audit_module>;
+class status_event_collector {
 public:
-    memory_audit_module(Core& core)
-        : module(core)
+    status_event_collector(Core& core)
+        : core_(core)
     {
+        core_.register_module(this);
     }
 
-    void handle_event(const allocation_event& event) {
-        (void)event;
-        std::cout << "allocate" << std::endl;
+    ~status_event_collector()
+    {
+        core_.unregister_module(this);
     }
 
-    void handle_event(const deallocation_event& event) {
-        (void)event;
-        std::cout << "free" << std::endl;
+    std::vector<status_event> query_status_events() {
+        try {
+            core_.emit_event(status_event_request{});
+        }
+        catch(const std::exception& ex) {
+            status_events_.push_back(status_event{
+                status_event_severity::error,
+                ex.what()
+            });
+        }
+
+        return std::move(status_events_);
     }
+
+    void handle_event(const status_event& evt) {
+        status_events_.push_back(evt);
+    }
+
+private:
+    Core& core_;
+    std::vector<status_event> status_events_;
 };
 
 namespace snw {
     template<>
-    struct subscription_list<memory_audit_module> {
+    struct subscription_list<status_event_collector> {
         using events = event_list<
-            allocation_event,
-            deallocation_event
+            status_event
+        >;
+    };
+}
+
+template<typename Core>
+class thing_a {
+public:
+    thing_a(Core& core)
+        : core_(core)
+    {
+        core_.register_module(this);
+    }
+
+    ~thing_a()
+    {
+        core_.unregister_module(this);
+    }
+
+    void handle_event(const status_event_request&) {
+        core_.emit_event(status_event{
+            status_event_severity::error,
+            "An error"
+        });
+    }
+
+private:
+    Core& core_;
+};
+
+namespace snw {
+    template<>
+    struct subscription_list<thing_a> {
+        using events = event_list<
+            status_event_request
+        >;
+    };
+}
+
+template<typename Core>
+class thing_b {
+public:
+    thing_b(Core& core)
+        : core_(core)
+    {
+        core_.register_module(this);
+    }
+
+    ~thing_b()
+    {
+        core_.unregister_module(this);
+    }
+
+    void handle_event(const status_event_request&) {
+        core_.emit_event(status_event{
+            status_event_severity::warning,
+            "A warning"
+        });
+    }
+
+private:
+    Core& core_;
+};
+
+namespace snw {
+    template<>
+    struct subscription_list<thing_b> {
+        using events = event_list<
+            status_event_request
         >;
     };
 }
 
 int main(int argc, char** argv) {
     using my_core = snw::core<
-        memory_module,
-        memory_audit_module
+        status_event_collector,
+        thing_a,
+        thing_b
     >;
 
     my_core core;
-    memory_module<my_core> memory(core);
-    memory_audit_module<my_core> memory_audit(core);
+    status_event_collector<my_core> sec{core};
+    thing_a<my_core> ta{core};
+    thing_b<my_core> tb{core};
 
-    // std::cout << memory.is_subscribed<allocation_event>() << std::endl;
-
-    {
-        auto chunk = memory.allocate(16);
-        memory.deallocate(chunk, 16);
+    for(const status_event& se: sec.query_status_events()) {
+        std::cout << se.msg << std::endl;
     }
 
 #ifdef SNW_OS_WINDOWS
