7bc67ba3a270edf80f367eb901746e8c8204f1a7
Sojourn <jsrausch.sojourn@gmail.com>
2018-08-26 10:43:03 -0500

    Fix linux build

--- src/libsnw_mem/page.h
+++ src/libsnw_mem/page.h
@@ -3,9 +3,7 @@
 namespace snw {
 
 struct page {
-    alignas(4096) uint8_t data[4096];
+    uint8_t data[4096];
 };
-static_assert(alignof(page) == 4096, "");
-static_assert(sizeof(page) == 4096, "");
 
 }
