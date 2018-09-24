720a751b667605b306e442f4278251bde826f7b8
Joel Rausch <jsrausch.sojourn@gmail.com>
2018-09-24 17:55:56 -0500

    alias core as event_router

--- src/snw/main.cpp
+++ src/snw/main.cpp
@@ -5,6 +5,12 @@
 #include "snw_util.h"
 #include "snw_core.h"
 
+namespace snw {
+    // alias this until it gets renamed
+    template<template<typename> class... Modules>
+    using event_router = core<Modules...>;
+}
+
 int main(int argc, char** argv) {
 
 #ifdef SNW_OS_WINDOWS
