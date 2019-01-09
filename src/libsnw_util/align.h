5ff6446a9bd5acb2ed609b7faec274450d59cf3e
Sojourn <jsrausch.sojourn@gmail.com>
2019-01-08 18:06:18 -0600

    Make align_up and align_down constexpr

--- src/libsnw_util/align.h
+++ src/libsnw_util/align.h
@@ -5,19 +5,19 @@
 namespace snw {
 
 template<typename T>
-inline T align_up(T value, size_t alignment)
+constexpr T align_up(T value, size_t alignment)
 {
-    auto n = static_cast<size_t>(value);
-    auto aligned_n = (alignment + (n-1)) & ~(n-1);
-    return static_cast<T>(aligned_n);
+    return static_cast<T>(
+        (static_cast<uintptr_t>(alignment) + (static_cast<uintptr_t>(value) - 1)) & ~(static_cast<uintptr_t>(value) - 1)
+    );
 }
 
 template<typename T>
-inline T align_down(T value, size_t alignment)
+constexpr T align_down(T value, size_t alignment)
 {
-    auto n = static_cast<size_t>(value);
-    auto aligned_n = (alignment & ~(n-1));
-    return static_cast<T>(aligned_n);
+    return static_cast<T>(
+        (static_cast<uintptr_t>(alignment) & ~(static_cast<uintptr_t>(value) - 1))
+    );
 }
 
 }
