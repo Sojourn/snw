76f99292b65965fbe3eb175b3eebeb8e26a35264
Sojourn <jsrausch.sojourn@gmail.com>
2019-02-13 20:17:24 -0600

    Add an integer hash function

---
+++ src/libsnw_util/hash.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+
+namespace snw {
+
+// stolen from https://nullprogram.com/blog/2018/07/31/
+inline uint32_t hash(uint32_t x) {
+    static constexpr uint32_t a = 0xed5ad4bb;
+    static constexpr uint32_t b = 0xac4c1b51;
+    static constexpr uint32_t c = 0x31848bab;
+
+    x ^= x >> 17;
+    x *= a;
+    x ^= x >> 11;
+    x *= b;
+    x ^= x >> 15;
+    x *= c;
+    x ^= x >> 14;
+
+    return x;
+}
+
+}
