2bf598bf8ffb089e716512400080ff6ebd40eb59
Sojourn <jsrausch.sojourn@gmail.com>
2019-05-04 09:07:38 -0500

    Expand suite of bit operations

--- src/libsnw_util/bits.h
+++ src/libsnw_util/bits.h
@@ -1,48 +1,84 @@
 #pragma once
 
+#include <cstdint>
 #include <cassert>
-#include "platform.h"
 
 namespace snw {
 
-inline uint32_t count_trailing_zeros(uint32_t value) {
-    assert(value);
-
-#ifdef SNW_OS_LINUX
-    static_assert(sizeof(value) == sizeof(unsigned int),
-        "Using the wrong builtin for this type");
-
-    return static_cast<uint32_t>(__builtin_ctz(value));
-
-#else
-    for (uint32_t i = 0; i < (sizeof(uint32_t) * 8); ++i) {
-        if (value & (static_cast<uint32_t>(1) << i)) {
-            return i;
-        }
-    }
-
-    return 0;
-#endif
+template<typename T>
+inline void set_bit(T& value, int index) {
+    value |= (static_cast<T>(1) << index);
 }
 
-inline uint64_t count_trailing_zeros(uint64_t value) {
-    assert(value);
+template<typename T>
+inline void clear_bit(T& value, int index) {
+    value &= ~(static_cast<T>(1) << index);
+}
 
-#ifdef SNW_OS_LINUX
-    static_assert(sizeof(value) == sizeof(unsigned long long),
-        "Using the wrong builtin for this type");
+template<typename T>
+inline void toggle_bit(T& value, int index) {
+    value ^= (static_cast<T>(1) << index);
+}
 
-    return static_cast<uint64_t>(__builtin_ctzll(value));
+template<typename T>
+inline bool test_bit(T value, int index) {
+    return value & (static_cast<T>(1) << index);
+}
 
-#else
-    for (uint32_t i = 0; i < (sizeof(uint64_t) * 8); ++i) {
-        if (value & (static_cast<uint64_t>(1) << i)) {
-            return i;
-        }
+inline int count_trailing_zeros(uint32_t value) {
+    return __builtin_ctz(value);
+}
+
+inline int count_trailing_zeros(uint64_t value) {
+    return __builtin_ctzll(value);
+}
+
+inline int count_set_bits(uint32_t value) {
+    return __builtin_popcount(value);
+}
+
+inline int count_set_bits(uint64_t value) {
+    return __builtin_popcountll(value);
+}
+
+inline int extract_set_bits(uint32_t value, int (&result)[32]) {
+    int set_bit_cnt = count_set_bits(value);
+    for (uint32_t i = 0; i < set_bit_cnt; ++i) {
+        int bit_index = count_trailing_zeros(value);
+        value &= ~(static_cast<uint32_t>(1) << bit_index);
+        result[i] = bit_index;
     }
 
-    return 0;
-#endif
+    return set_bit_cnt;
+}
+
+inline int extract_set_bits(uint64_t value, int (&result)[64]) {
+    int set_bit_cnt = count_set_bits(value);
+    for (uint64_t i = 0; i < set_bit_cnt; ++i) {
+        int bit_index = count_trailing_zeros(value);
+        value &= ~(static_cast<uint64_t>(1) << bit_index);
+        result[i] = bit_index;
+    }
+
+    return set_bit_cnt;
+}
+
+template<typename F>
+inline void for_each_set_bit(uint32_t value, F&& f) {
+    int result[32];
+    int set_bit_cnt = extract_set_bits(value, result);
+    for (int i = 0; i < set_bit_cnt; ++i) {
+        f(result[i]);
+    }
+}
+
+template<typename F>
+inline void for_each_set_bit(uint64_t value, F&& f) {
+    int result[64];
+    int set_bit_cnt = extract_set_bits(value, result);
+    for (int i = 0; i < set_bit_cnt; ++i) {
+        f(result[i]);
+    }
 }
 
 }
