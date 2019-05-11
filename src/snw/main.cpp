99f4e33648d5b8c8638368547c1f07db1e076e93
Sojourn <jsrausch.sojourn@gmail.com>
2019-05-11 10:08:24 -0500

    First cut at seeking to the first set/clear bit

--- src/snw/main.cpp
+++ src/snw/main.cpp
@@ -33,26 +33,32 @@
 namespace snw {
 
 struct bit_tree_node {
-    static const constexpr size_t capacity = 64 * 8; // 512
-    static const constexpr size_t log2_capacity = 9; // log2(512) == 9
+    static const constexpr size_t chunk_count = 4;
+    static const constexpr size_t capacity = 64 * chunk_count; // 512
+    static const constexpr size_t log2_capacity = 8; // log2(256) == 8
     static_assert(capacity == (1 << log2_capacity), "capacity and log2_capacity are out of sync");
+};
 
-    uint64_t bits[8];
+// these have aggregate information about child nodes
+struct alignas(64) bit_tree_branch_node : bit_tree_node {
+    uint64_t any_set_chunks[chunk_count];
+    uint64_t any_clear_chunks[chunk_count];
 
-    bit_tree_node() {
-        clear();
+    bit_tree_branch_node() {
+        for (size_t i = 0; i < chunk_count; ++i) {
+            any_set_chunks[i] = std::numeric_limits<uint64_t>::min();
+            any_clear_chunks[i] = std::numeric_limits<uint64_t>::max();
+        }
     }
+};
 
-    void clear() {
-        memset(bits, 0, sizeof(bits));
-    }
+struct alignas(16) bit_tree_leaf_node : bit_tree_node {
+    uint64_t chunks[chunk_count];
 
-    void clear_bit(size_t bit_index) {
-        bits[bit_index >> 6] &= ~(1ull << (bit_index & 63));
-    }
-
-    void set_bit(size_t bit_index) {
-        bits[bit_index >> 6] |= (1ull << (bit_index & 63));
+    bit_tree_leaf_node() {
+        for (size_t i = 0; i < chunk_count; ++i) {
+            chunks[i] = std::numeric_limits<uint64_t>::min();
+        }
     }
 };
 
@@ -64,15 +70,24 @@ struct bit_tree_level {
 
 struct bit_tree_layout {
     static const constexpr size_t max_height = 4;
-    static const constexpr size_t max_capacity = 1ull << (max_height * bit_tree_node::log2_capacity); // 64 << 30
+    static const constexpr size_t max_internal_height = max_height - 1;
+    static const constexpr size_t max_capacity = 1ull << 31;
+
+    // sanity check the constants
+    static_assert((1ull << (max_height * bit_tree_node::log2_capacity)) >= max_capacity, "increase height");
+    static_assert(std::numeric_limits<int32_t>::max() >= (max_capacity - 1), "index type is too small for max_capacity");
 
     size_t                            capacity;
-    size_t                            total_nodes;
+    size_t                            node_count;
+    size_t                            leaf_node_count;
+    size_t                            branch_node_count;
     array<bit_tree_level, max_height> levels;
 
     bit_tree_layout(size_t min_capacity)
         : capacity(min_capacity)
-        , total_nodes(0)
+        , node_count(0)
+        , leaf_node_count(0)
+        , branch_node_count(0)
     {
         // sanity check that we aren't trying to create a crazy huge layout
         if (min_capacity > max_capacity) {
@@ -91,17 +106,24 @@ struct bit_tree_layout {
         {
             bit_tree_level level;
             auto add_level = [&]() {
-                total_nodes += level.node_count;
+                node_count += level.node_count;
+                if (levels.empty()) {
+                    leaf_node_count += level.node_count;
+                }
+                else {
+                    branch_node_count += level.node_count;
+                }
+
                 levels.push_back(level);
             };
 
-            // add the bottom level first
+            // add a level of leaves
             level.node_count = capacity / bit_tree_node::capacity;
             level.shift = 0;
             level.mask = (bit_tree_node::capacity - 1) << level.shift;
             add_level();
 
-            // add levels until we get one with a single, root node
+            // add levels of branches until we find the root node
             while (level.node_count > 1) {
                 level.node_count = align_up(level.node_count, bit_tree_node::capacity) / bit_tree_node::capacity;
                 level.shift += bit_tree_node::log2_capacity;
@@ -125,9 +147,7 @@ struct bit_tree_layout {
     friend std::ostream& operator<<(std::ostream& out, const bit_tree_layout& layout) {
         out << "bit_tree_layout {\n";
         out << "  capacity: " << layout.capacity << ",\n";
-        out << "  total_nodes: " << layout.total_nodes << ",\n";
-        out << "  approximate_memory_usage: " << (layout.total_nodes * sizeof(bit_tree_node)) << ",\n";
-        out << "  approximate_bits_per_value: " << (float(8 * layout.total_nodes * sizeof(bit_tree_node)) / float(layout.capacity)) << ",\n";
+        out << "  node_count: " << layout.node_count << ",\n";
         out << "  levels: [\n";
         for (const bit_tree_level& level: layout.levels) {
             out << "    { node_count: " << level.node_count << ", shift: " << level.shift << ", mask: " << level.mask << " },\n";
@@ -137,24 +157,184 @@ struct bit_tree_layout {
     }
 };
 
-class bit_tree {
+// identifies a bit within a bit tree
+class bit_tree_index {
 public:
-    bit_tree(size_t min_capacity)
-        : layout_(min_capacity)
+    bit_tree_index(uint32_t index = 0)
+        : index_(index)
     {
-        nodes_.resize(layout_.total_nodes);
-        
-        size_t offset = 0;
-        for (const bit_tree_level& level: layout_.levels) {
-            levels_.push_back(&nodes_[offset]);
-            offset += level.node_count;
-        }
+    }
+
+    void push_node(int chunk, int bit) {
+        index_ = (index_ << 8) | (chunk << 6) | (bit << 0);
+    }
+
+    void pop_node() {
+        index_ = index_ >> 8;
+    }
+
+    int top_chunk() const {
+        return 3 & (index_ >> 6);
+    }
+
+    int top_bit() const {
+        return 63 & (index_ >> 0);
+    }
+
+    uint32_t get() const {
+        return index_;
     }
 
 private:
-    bit_tree_layout                                    layout_;
-    array<bit_tree_node*, bit_tree_layout::max_height> levels_;
-    std::vector<bit_tree_node>                         nodes_;
+    uint32_t index_;
+};
+
+class bit_tree {
+public:
+    bit_tree(size_t min_capacity)
+        : size_(0)
+        , layout_(min_capacity)
+    {
+        leaves_.resize(layout_.leaf_node_count);
+        branches_.resize(layout_.branch_node_count);
+
+        // setup branch index
+        size_t offset = 0;
+        for (size_t i = 0; i < (layout_.levels.size() - 1); ++i) {
+            branch_index_.push_back(&branches_[offset]);
+            offset += layout_.levels[i].node_count;
+        }
+    }
+
+    int32_t add() {
+        return -1;
+    }
+
+    void remove(int32_t index) {
+    }
+
+    template<typename F>
+    void scan(F&& f) /* const */ {
+        // todo
+    }
+
+    size_t size() const {
+        return size_;
+    }
+
+    size_t capacity() const {
+        return layout_.capacity;
+    }
+
+    bool full() const {
+        return size() == capacity();
+    }
+
+    bool empty() const {
+        return size() == 0;
+    }
+
+private:
+    enum class cursor_mode {
+        set,
+        clear,
+    };
+
+    template<cursor_mode mode>
+    class cursor {
+    public:
+        cursor(bit_tree& bt)
+            : bt_(bt)
+        {
+            if (mode == cursor_mode::set) {
+                assert(!bt_.empty());
+            }
+            else {
+                assert(!bt_.full());
+            }
+
+            {
+                // traverse the branch level
+                size_t height = bt_.layout_.levels.size();
+                for (size_t current_depth = 0; current_depth < (height - 1); ++current_depth) {
+                    const bit_tree_branch_node& branch = bt_.branch_index_[current_depth][index_.get()];
+                    const uint64_t* chunks;
+                    if (mode == cursor_mode::set) {
+                        chunks = branch.any_set_chunks;
+                    }
+                    else {
+                        chunks = branch.any_clear_chunks;
+                    }
+
+                    // scan chunks
+                    for (size_t chunk_index = 0; chunk_index < bit_tree_node::chunk_count; ++chunk_index) {
+                        uint64_t chunk = chunks[chunk_index];
+                        if (chunk) {
+                            int bit_index = count_trailing_zeros(chunk);
+                            index_.push_node(static_cast<int>(chunk_index), bit_index);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // traverse the leaf level
+            {
+                const bit_tree_leaf_node& leaf = bt_.leaves_[index_.get()];
+
+                // scan chunks
+                for (size_t chunk_index = 0; chunk_index < bit_tree_node::chunk_count; ++chunk_index) {
+                    uint64_t chunk = leaf.chunks[chunk_index];
+                    if (mode == cursor_mode::clear) {
+                        chunk = ~chunk; // invert chunk because we're looking for clear bits
+                    }
+                    if (chunk) {
+                        int bit_index = count_trailing_zeros(chunk);
+                        index_.push_node(static_cast<int>(chunk_index), bit_index);
+                        break;
+                    }
+                }
+            }
+        }
+
+        cursor(bit_tree& bt, uint32_t index)
+            : bt_(bt)
+            , index_(index)
+        {
+            if (mode == cursor_mode::set) {
+                assert(bt.size() > 0);
+            }
+            else {
+                assert(bt.size() < bt.capacity());
+            }
+
+            assert(index < bt.capacity());
+        }
+
+        int32_t next() {
+            return -1;
+        }
+
+    private:
+        struct frame {
+            size_t node_offset;
+            size_t chunk_offset;
+            size_t bit_offset;
+        };
+
+    private:
+        bit_tree      &bt_;
+        bit_tree_index index_;
+    };
+
+private:
+    using branch_index = array<bit_tree_branch_node*, bit_tree_layout::max_internal_height>;
+
+    size_t                            size_;
+    bit_tree_layout                   layout_;
+    branch_index                      branch_index_;
+    std::vector<bit_tree_leaf_node>   leaves_;
+    std::vector<bit_tree_branch_node> branches_;
 };
 
 }
